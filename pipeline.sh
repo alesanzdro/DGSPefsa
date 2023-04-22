c72e27d6fd2650b21e5a5d49de3e1ddbdd474097
alesanzdro <alexsanzc@gmail.com>
2023-04-22 17:02:17 +0200

    Building pipeline, firts controls

--- pipeline.sh
+++ pipeline.sh
@@ -16,7 +16,12 @@ export TRIMMOMATIC_ADAPTERS=${RESOURCES_PATH}"/trimmomatic/adapters"
 DATABASE_KMERFINDER="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/kmerfinder/databases/bacteria/bacteria"
 
 
-#export PERL5LIB=/software/miniconda3/envs/dgsp_amr_mlst_detection/lib/perl5/5.32
+export PERL5LIB=/software/miniconda3/envs/dgsp_efsa_sp/lib/perl5/5.32
+export PATH=/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/signalp-5.0b/bin:$PATH
+export PATH="${CONDAPATH}/dgsp_efsa_contamination/INNUca:${CONDAPATH}/dgsp_efsa_contamination/ReMatCh/ReMatCh:$PATH"
+
+MASH_DB="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/mash/refseq.genomes.k21s1000.msh"
+#MASH_DB="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/mash/RefSeq88n.msh"
 
 
 ################################################################################
@@ -48,7 +53,8 @@ IR=${INPUT_PATH}"/"${RUN}"/"${SRUN}
 DATE=$(date +"%y%m%d")
 OUTPUT_PATH="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/analysis/"${RUN}
 CONDAPATH="/software/miniconda3/envs"
-THREADS=24
+BACTPIPE="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/BACTpipe-2.7.0/bactpipe.nf"
+THREADS=16
 SPADESMEM=72
 
 
@@ -64,15 +70,22 @@ SPADESMEM=72
 ##########################################################
 
 if [[ ! -e ${OUTPUT_PATH} ]]; then
-    mkdir -p ${OUTPUT_PATH}"/stats/fastqc/raw"
-    mkdir -p ${OUTPUT_PATH}"/stats/fastqc/trim"
-    mkdir -p ${OUTPUT_PATH}"/stats/fastp"
-    mkdir -p ${OUTPUT_PATH}"/stats/mash"
-    mkdir -p ${OUTPUT_PATH}"/out/0_fastq"
+    # En la carpeta pipeline es donde correremos innuca, bactpipe, confindr... los controles de calidad
+    mkdir -p ${OUTPUT_PATH}"/pipeline"
+
+    mkdir -p ${OUTPUT_PATH}"/reports/fastqc/raw"
+    mkdir -p ${OUTPUT_PATH}"/reports/fastqc/trim"
+    mkdir -p ${OUTPUT_PATH}"/reports/fastp"
+
     mkdir -p ${OUTPUT_PATH}"/logs/fastp"
     mkdir -p ${OUTPUT_PATH}"/logs/efsa"
-    mkdir -p ${OUTPUT_PATH}"/logs/mash"
-    mkdir -p ${OUTPUT_PATH}"/report"
+    mkdir -p ${OUTPUT_PATH}"/logs/BACTpipe"
+    mkdir -p ${OUTPUT_PATH}"/logs/innuca"
+    mkdir -p ${OUTPUT_PATH}"/logs/confindr"
+    mkdir -p ${OUTPUT_PATH}"/logs/checkm"
+
+    innuca
+   
     mkdir -p ${OUTPUT_PATH}"/tmp"
     elif [[ ! -d ${OUTPUT_PATH} ]]; then
     echo "${OUTPUT_PATH} already exists but is not a directory" 1>&2
@@ -97,6 +110,26 @@ fi
     do
         echo "$sample FASTQ1: $fastq_1 FASTQ2: $fastq_2"
 
+
+        # VARIABLES
+        VAR_C1_LENGTH=301
+        VAR_C1_GENOME=2880000
+        VAR_C2_SPE="Listeria monocytogenes"
+        VAR_C3_GENOME=$(echo "scale=3; $VAR_C1_GENOME/1000000" | bc -l)
+
+
+        # DIRECTORIOS ASOCIADOS A MUESTRA / ESPECIE
+        mkdir -p ${OUTPUT_PATH}"/pipeline/"${sample}"/fastq"
+        mkdir -p ${OUTPUT_PATH}"/pipeline/"${sample}"/assembly"
+        
+        if [[ "$VAR_C2_SPE" == "Salmonella enterica" ]];then
+            #mkdir -p ${OUTPUT_PATH}"/logs/seqsero2"
+            #mkdir -p ${OUTPUT_PATH}"/pipeline/"${sample}"/seqsero2"
+        elif [[ "$VAR_C2_SPE" == "Listeria monocytogenes" ]]; then
+            mkdir -p ${OUTPUT_PATH}"/logs/lissero"
+            #mkdir -p ${OUTPUT_PATH}"/pipeline/"${sample}"/lissero"
+        fi
+
         ##########################################################
         #
         # 02  Assessment of the genomic sequence quality
@@ -113,7 +146,7 @@ fi
         ################################################################################
         # FastQC Quality RAW
         ################################################################################
-        fastqc --quiet --threads ${THREADS} -outdir ${OUTPUT_PATH}/stats/fastqc/raw ${IR}/${fastq_1} ${IR}/${fastq_2}
+        fastqc --quiet --threads ${THREADS} -outdir ${OUTPUT_PATH}/reports/fastqc/raw ${IR}/${fastq_1} ${IR}/${fastq_2}
 
         
         ################################################################################
@@ -121,8 +154,8 @@ fi
         ################################################################################
         fastp --thread ${THREADS} --in1  ${IR}/${fastq_1} --in2 ${IR}/${fastq_2} \
         --cut_tail --cut_window_size=10 --cut_mean_quality=20 --length_required=50 --correction \
-        --json ${OUTPUT_PATH}/stats/fastp/${sample}.report.fastp.json --html ${OUTPUT_PATH}/stats/fastp/${sample}.report.fastp.html \
-        --out1 ${OUTPUT_PATH}/out/0_fastq/${sample}_1.fastq.gz --out2 ${OUTPUT_PATH}/out/0_fastq/${sample}_2.fastq.gz  >> ${OUTPUT_PATH}/logs/fastp/${sample}.log 2>&1
+        --json ${OUTPUT_PATH}/reports/fastp/${sample}.report.fastp.json --html ${OUTPUT_PATH}/reports/fastp/${sample}.report.fastp.html \
+        --out1 ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_1.fastq.gz --out2 ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_2.fastq.gz  >> ${OUTPUT_PATH}/logs/fastp/${sample}.log 2>&1
    
         ################################################################################
         # CHECK Q30 EFSA
@@ -133,8 +166,8 @@ fi
         # si la longitud media de lectura = (2 × 76 pb) y Q30 < 85 %, pipeline fallará debido a la baja tasa de Q30;
         # si el número total de bases es menor que el tamaño del genoma del patógeno multiplicado por el umbral mínimo de cobertura (30x), pipeline fallará debido a una cobertura insuficiente después del recorte.
         
-        VAR_C1_LENGTH=301
-        VAR_C1_GENOME=2880000
+
+
         # Recuperamos valor Q30
 
         BASESQ30=$(grep "Q30" ${OUTPUT_PATH}/logs/fastp/${sample}.log | tail -n 2 | awk -F " " '{print $3}' | awk -F "(" '{print $1}' | awk '{total += $1}END{ printf total}')
@@ -179,38 +212,178 @@ fi
         printf 'STATUS: %s\n' "$STATUSQ30"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
 
         if [ "$STATUSQ30" = "PASS" ]; then
-        ################################################################################
-        # FastQC Quality TRIM
-        ################################################################################
-        fastqc --quiet --threads ${THREADS} --outdir ${OUTPUT_PATH}/stats/fastqc/trim ${OUTPUT_PATH}/out/0_fastq/${sample}_1.fastq.gz ${OUTPUT_PATH}/out/0_fastq/${sample}_2.fastq.gz
-        conda deactivate    
+            ################################################################################
+            # FastQC Quality TRIM
+            ################################################################################
+            fastqc --quiet --threads ${THREADS} --outdir ${OUTPUT_PATH}/reports/fastqc/trim ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_1.fastq.gz ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_2.fastq.gz
+            conda deactivate    
 
-        ##########################################################
-        #
-        # 03 Species validation
-        #
-        ##########################################################
-        conda activate ${CONDAPATH}/dgsp_efsa_sp
-        
-        RefSeq88n.msh  refseq.genomes.k21s1000.msh
+            ##########################################################
+            #
+            # 03 Species validation
+            #
+            ##########################################################
+            conda activate ${CONDAPATH}/dgsp_efsa_sp
 
-        #MASH_DB="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/mash/RefSeq88n.msh"
-        MASH_DB="/ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/mash/refseq.genomes.k21s1000.msh"
-        mash screen ${MASH_DB} ${OUTPUT_PATH}/out/0_fastq/${sample}_1.fastq.gz ${OUTPUT_PATH}/out/0_fastq/${sample}_2.fastq.gz > ${OUTPUT_PATH}/logs/mash/${sample}_88.txt
+            #mash screen ${MASH_DB} ${OUTPUT_PATH}/out/0_fastq/${sample}_1.fastq.gz ${OUTPUT_PATH}/out/0_fastq/${sample}_2.fastq.gz > ${OUTPUT_PATH}/logs/mash/${sample}_88.txt
+       
+            cd ${OUTPUT_PATH}/pipeline/${sample}
+            nextflow-20.10.0-all run ${BACTPIPE} \
+            --mashscreen_database ${MASH_DB} \
+            --reads './fastq/*_{1,2}.fastq.gz' >> ${OUTPUT_PATH}/logs/BACTpipe/${sample}.log 2>&1
 
-        #nextflow run ctmrbio/BACTpipe --mashscreen_database path/to/refseq.genomes.k21s1000.msh --reads '*_R{1,2}.fastq.gz'
-        nextflow run ctmrbio/BACTpipe --mashscreen_database ${MASH_DB} --reads ${OUTPUT_PATH}/out/0_fastq/${sample}_{1,2}.fastq.gz
+            echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+            echo "SPECIES CHECK" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+            echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+            cat ${OUTPUT_PATH}/pipeline/${sample}/BACTpipe_results/mash_screen/all_samples.mash_screening_results.tsv >> ${OUTPUT_PATH}/logs/efsa/${sample}.log
+            C2_STATUS=$(cat ${OUTPUT_PATH}/pipeline/${sample}/BACTpipe_results/mash_screen/all_samples.mash_screening_results.tsv | awk '{print $2}') 
+            C2_SPE=$(cat ${OUTPUT_PATH}/pipeline/${sample}/BACTpipe_results/mash_screen/all_samples.mash_screening_results.tsv | awk -F "\t" '{print $5}' | sed -e "s/\[\|\]\|'//g") 
 
-        RefSeq88n.msh
-nextflow run /ALMEIDA/PROJECTS/BACTERIAS/DGSP/resources/BACTpipe-2.7.0/bactpipe.nf --mashscreen_database ${MASH_DB} --reads '/ALMEIDA/PROJECTS/BACTERIAS/DGSP/analysis/230210_LSPV002/out/0_fastq/22_LM_03313_{1,2}.fastq.gz' --outdir p
+            printf 'OUR SAMPLE: %s\n' "$VAR_C2_SPE"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+
+
+            if [[ "$C2_STATUS" = "PASS" ]]  && [[ "$C2_SPE" == "$VAR_C2_SPE" ]]; then     
+                STATUSBACT="PASS"
+                printf 'STATUS: %s\n' "$STATUSBACT"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+
+                # CHECK SEROVARES !!!!!
+                if [[ "$VAR_C2_SPE" = "Salmonella enterica" ]];then
+                    # Raw reads k-mer ("-m k"), for separated paired-end raw reads ("-t 2")
+                    SeqSero2_package.py -m k -t 2 -i ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_1.fastq.gz ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_2.fastq.gz >> ${OUTPUT_PATH}/pipeline/${sample}/${sample}.seqsero2.txt 2>&1
+                fi
+
+                if [[ "$VAR_C2_SPE" = "Listeria monocytogenes" ]];then
+                    (cd ${OUTPUT_PATH}/pipeline/${sample}/BACTpipe_results/shovill && lissero ${sample}.contigs.fa >> ${OUTPUT_PATH}/pipeline/${sample}/${sample}.lissero.txt 2>&1)
+                    var2erase=$(echo ${OUTPUT_PATH}/pipeline/${sample}/BACTpipe_results/shovill/)
+                    #IMPORTANTE EN SED SE EMPLEA ":" PARA SEPARAS CAMPOS Y PODER ELIMINAR PATH PARCIAL
+                    lissero ${OUTPUT_PATH}/pipeline/${sample}/BACTpipe_results/shovill/${sample}.contigs.fa --logfile ${OUTPUT_PATH}/logs/lissero/${sample}.log | sed "s:^$var2erase::" > ${OUTPUT_PATH}/pipeline/${sample}/${sample}.lissero.txt 
+                fi
+
+                ##########################################################
+                #
+                # 04 Contamination
+                #
+                ##########################################################
+                echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                echo "Contamination CHECK" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                confindr.py -i ${OUTPUT_PATH}/pipeline/${sample}/fastq -o ConFindr >> ${OUTPUT_PATH}/logs/confindr/${sample}.log 2>&1
+                conda deactivate
+
+                conda activate ${CONDAPATH}/dgsp_efsa_contamination
+
+                INNUca.py -j ${THREADS} \
+                -i ${OUTPUT_PATH}/pipeline/${sample}/fastq \
+                -o INNUca \
+                -s "$VAR_C2_SPE" -g $VAR_C3_GENOME >> ${OUTPUT_PATH}/logs/innuca/${sample}.log 2>&1
+
+                # Recogemos resultados contaminación
+                C3_confindr=$(awk -F "," '{print $4}' ${OUTPUT_PATH}/pipeline/${sample}/ConFindr/confindr_report.csv | grep -v "ContamStatus" | uniq)
+                C3_warning=$(awk -F "," '{print $3}' ${OUTPUT_PATH}/pipeline/${sample}/ConFindr/confindr_report.csv | grep -v "NumContamSNVs" | awk '{total += $1; count += 1}END{ printf "%4.3f\n",  total / count}')
+                
+                #Si el número de SNV contaminados supera los siguientes umbrales específicos de especies 
+                #(como lo sugiere Deneke et al., 2021a), se envía un mensaje de advertencia:
+                #• S. enterica: 7
+                #• L. monocytogenes: 3
+                #• E.coli: 4
+                if [[ "$VAR_C2_SPE" = "Listeria monocytogenes" ]];then
+                    if (( $(echo "$C3_warning > 3" | bc -l) ));then
+                        CONTSNV="FAIL" 
+                    else
+                        CONTSNV="PASS" 
+                    fi
+                elif [[ "$VAR_C2_SPE" = "Salmonella enterica" ]];then
+                    if (( $(echo "$C3_warning > 7" | bc -l) )); then
+                        CONTSNV="FAIL" 
+                    else
+                        CONTSNV="PASS" 
+                    fi
+                elif [[ "$VAR_C2_SPE" = "Escherichia coli" ]];then
+                    if (( $(echo "$C3_warning > 4" | bc -l) )); then 
+                        CONTSNV="FAIL" 
+                    else
+                        CONTSNV="PASS" 
+                    fi
+                else
+                    if (( $(echo "$C3_warning > 4" | bc -l) )); then     
+                        CONTSNV="FAIL" 
+                    else
+                        CONTSNV="PASS" 
+                    fi
+                fi
+
+
+                if [[ "$CONTSNV" = "FAIL" ]]; then 
+                    echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    echo "========================================" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    echo "WARNING!" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    echo "ConFindr show SNV contamination" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    echo "========================================" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                else
+
+                repinnuca=$(ls ${OUTPUT_PATH}/pipeline/${sample}/INNUca/samples_report.*)
+                C3_innuca=$(awk -F "\t" '{print $3}' $repinnuca  | grep -v "samples_passQC")
+
+                if [[ "$C3_confindr" = "False" ]]  && [[ "$C3_innuca" = "PASS" ]]; then     
+                    STATUSCONT="PASS"
+                    printf 'STATUS: %s\n' "$STATUSCONT"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+
+                ##########################################################
+                #
+                # 05 Assembly
+                #
+                ##########################################################
+                conda activate ${CONDAPATH}/dgsp_efsa_sp
+                
+                shovill --depth 100 --kmers 31,33,55,77,99,127 --minlen 500 --cpus ${THREADS} \
+                --R1 ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_1.fastq.gz \
+                --R2 ${OUTPUT_PATH}/pipeline/${sample}/fastq/${sample}_1.fastq.gz \
+                --outdir ${OUTPUT_PATH}"/pipeline/"${sample}"/assembly/shovill"
+                
+                cp ${OUTPUT_PATH}/pipeline/${sample}/assembly/shovill/contigs.fa ${OUTPUT_PATH}/pipeline/${sample}/assembly/${sample}.contigs.fa
+                statswrapper.sh in=${OUTPUT_PATH}/pipeline/${sample}/assembly/${sample}.contigs.fa > ${OUTPUT_PATH}/pipeline/${sample}/assembly/${sample}.assembly_stats.txt
+
+                echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                echo "Assembly quality CHECK" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                mkdir -p ${OUTPUT_PATH}/pipeline/${sample}/assembly/contigs
+                #Desglosamos fichero contigs en fastas individuales
+                #(cd ${OUTPUT_PATH}/pipeline/${sample}/assembly/contigs && cat ../${sample}.contigs.fa | awk '{
+                (cd ${OUTPUT_PATH}/pipeline/${sample}/assembly/contigs && cat ../../BACTpipe_results/shovill/${sample}.contigs.fa | awk '{
+                        if (substr($0, 1, 1)==">") {filename=(substr($1,2) ".fa")}
+                        print $0 >> filename
+                        close(filename)
+                }'
+                )
+
+                checkm lineage_wf -t ${THREADS} \
+                -x fa ${OUTPUT_PATH}/pipeline/${sample}/assembly/contigs \
+                ${OUTPUT_PATH}/pipeline/${sample}/checkm >> ${OUTPUT_PATH}/logs/checkm/${sample}.log 2>&1
+            
+                #----------------------------------------------------------------------------------------------------------------------------------------------------------
+                #  genes    c__Bacilli (UID354)      515         328           182        1   314   12   1   0   0       99.45            5.22              86.67          
+                #----------------------------------------------------------------------------------------------------------------------------------------------------------
+             
+                #revisar /software/miniconda3/envs/dgsp_efsa_sp/checkm_data
+ 
+                cd /ALMEIDA/PROJECTS/BACTERIAS/DGSP/analysis/230210_LSPV002/pipeline/22_LM_03313/checkm_test
+                checkm lineage_wf -t ${THREADS} -x fa bins/22_LM_03313/ salida
+                else
+                    echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    printf '%s FAILED IN CONTAMINATION CHECK\n' "$sample"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                    conda deactivate
+                fi
 
 
 
-        #https://mash.readthedocs.io/en/latest/data.html
+            else
+                echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                printf '%s FAILED IN SPECIES CHECK\n' "$sample"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
+                conda deactivate
+            fi
 
-        wget ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt 
-
-        conda deactivate    
         else
             echo "****************************************" | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
             printf '%s FAILED IN Q30 CHECK\n' "$sample"  | tee -a ${OUTPUT_PATH}/logs/efsa/${sample}.log
